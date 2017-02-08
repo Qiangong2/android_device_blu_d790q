0bec019b98ec8bd39ba6f8449533393e3bbaf244
ZhongQian TiaoGong <sassafrass@fasizzle.com>
2017-02-08 14:16:07 -0800

    Various updates
    Remove problematic stock overlays, add hardware configs

--- lineage.mk
+++ lineage.mk
@@ -10,6 +10,8 @@ $(call inherit-product, device/blu/d790q/device.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+PRODUCT_LOCALES := en_US es_US es_ES pt_PT pt_BR nl_NL zh_CN fr_FR de_DE el_GR hu_HU cs_CZ pl_PL ar_EG fa_IR sr_RS ru_RU iw_IL bg_BG vi_VN hr_HR zh_TW th_TH ja_JP tl_PH in_ID my_MM bn_BD ms_MY
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_d790q
 PRODUCT_BRAND := BLU
